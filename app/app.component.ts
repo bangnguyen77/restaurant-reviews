c5b5f74aa8bca6fdf49f982fe42eb06022aace24
Bang Nguyen and Noah Ramey <bang@>
2016-08-10 11:42:25 -0700

    display list of restaurants

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from 'angular2/core';
+import { RestaurantListComponent } from './restaurant-list.component';
+import { Restaurant } from './restaurant.model';
 
 @Component({
   selector: 'my-app',
+  directives: [RestaurantListComponent],
   template: `
-    <h1>Skeleton Angular2 App!</h1>
+    <div class="container">
+      <h1>Restaurant Reviews</h1>
+      <restaurant-list [restaurantList]="restaurants">
+      </restaurant-list>
+    </div>
   `
 })
-
 export class AppComponent {
-
+  public restaurants: Restaurant[];
+  constructor(){
+    this.restaurants = [
+      new Restaurant('test-name', 'test-specialty', 'test-address', 1, 0),
+      new Restaurant('test-name2', 'test-specialty2', 'test-address2', 2, 1),
+      new Restaurant('test-name3', 'test-specialty3', 'test-address3', 3, 2)
+    ]
+  }
 }
---
+++ app/restaurant.component.ts
@@ -0,0 +1,19 @@
+import { Component } from 'angular2/core';
+import { Restaurant } from './restaurant.model';
+
+@Component({
+  selector: 'restaurant-display',
+  inputs: ['restaurant'],
+  template: `
+    <div>
+      <h3>{{ restaurant.name }}</h3>
+      <p>{{ restaurant.specialty }}</p>
+      <p>{{ restaurant.address }}</p>
+      <p>{{ restaurant.price }}</p>
+      <p>{{ restaurant.rating }}</p>
+    </div>
+  `
+})
+export class RestaurantComponent {
+  public restaurant: Restaurant;
+}
